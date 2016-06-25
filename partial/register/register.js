155e4edd597d7d06f03f855ee506626a7abf31c7
Diego Haz <hazdiego@gmail.com>
2016-06-25 18:45:46 -0300

    Enhance register

--- partial/register/register.js
+++ partial/register/register.js
@@ -1,6 +1,6 @@
 angular.module('platformSdk').controller('RegisterCtrl',function($scope, UserService){
-    $scope.signUp = function(email, password) {
-        return UserService.signUp(email, password).then(function(user) {
+    $scope.signUp = function(name, surname, username, email, password) {
+        return UserService.signUp(name, surname, username, email, password).then(function(user) {
             console.log(user)
         }).catch(function(error) {
             console.log(error)
--- service/user.js
+++ service/user.js
@@ -5,8 +5,15 @@ angular.module('platformSdk').factory('UserService',function() {
         return Parse.User.current();
     };
 
-    user.signUp = function(username, password) {
-        return Parse.User.signUp(username, password);
+    user.signUp = function(name, surname, username, email, password) {
+        var user = new Parse.User();
+        user.set('name', name);
+        user.set('surname', surname);
+        user.set('username', username);
+        user.set('email', email);
+        user.set('password', password);
+
+        return user.signUp();
     };
 
     user.logIn = function(username, password) {
