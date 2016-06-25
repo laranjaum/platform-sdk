f6f1cba2e69a5c55546d3de915fd2be50e8588f2
Diego Haz <hazdiego@gmail.com>
2016-06-25 19:56:30 -0300

    Enhance register form add validation

--- partial/register/register.js
+++ partial/register/register.js
@@ -1,4 +1,8 @@
 angular.module('platformSdk').controller('RegisterCtrl',function($scope, UserService){
+    $scope.match = function(a, b) {
+        return a === b;
+    }
+
     $scope.signUp = function(name, surname, username, email, password) {
         return UserService.signUp(name, surname, username, email, password).then(function(user) {
             console.log(user)
