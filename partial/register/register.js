783b61868da79161afe97d8e0ec81b374a751035
Diego Haz <hazdiego@gmail.com>
2016-06-26 10:44:18 -0300

    Add duplicate email validation to register

--- partial/register/register.js
+++ partial/register/register.js
@@ -1,13 +1,44 @@
-angular.module('platformSdk').controller('RegisterCtrl',function($scope, UserService){
+angular.module('platformSdk').controller('RegisterCtrl',function($scope, $state, UserService){
     $scope.match = function(a, b) {
         return a === b;
-    }
+    };
+
+    $scope.emailTaken = function(email) {
+        UserService.getByEmail(email).then(function(user) {
+            if (user) {
+                $scope.registerForm.emailInput.$error.duplicate = true;
+            } else {
+                $scope.registerForm.emailInput.$setPristine();
+            }
+        });
+    };
+
+    $scope.usernameTaken = function(username) {
+        UserService.getByUsername(username).then(function(user) {
+            if (user) {
+                $scope.registerForm.usernameInput.$error.duplicate = true;
+            } else {
+                $scope.registerForm.usernameInput.$setPristine();
+            }
+        });
+    };
 
     $scope.signUp = function(name, surname, username, email, password) {
         return UserService.signUp(name, surname, username, email, password).then(function(user) {
-            console.log(user)
+            console.log(user);
+            $state.go('initiatives');
+            $scope.registerForm.$setUntouched();
         }).catch(function(error) {
-            $scope.registerForm.usernameInput.$error.duplicate = true
-        })
+            switch (error.code) {
+                case Parse.Error.DUPLICATE_VALUE:
+                    $scope.registerForm.usernameInput.$error.duplicate = true;
+                    break;
+                case Parse.Error.EMAIL_TAKEN:
+                    $scope.registerForm.emailInput.$error.duplicate = true;
+                    break;
+                default:
+                    $scope.registerForm.$setUntouched();
+            }
+        });
     };
 });
